a09a8ada0058244309ef9dceb5dc9f3588db3877
Ricardo Carneiro <rjorge.carneiro@gmail.com>
2016-07-13 19:23:45 +0100

    added exit code.. still, long way to go...

--- BuildAndReleaseApk.ps1
+++ BuildAndReleaseApk.ps1
@@ -6,23 +6,25 @@ $android = "C:\Program Files (x86)\Android\android-sdk\build-tools"
 $outputApkName = "MyApkName.apk"
 
 Clear-Host
-Remove-Item -Recurse $path 
-cmd /c ionic build android --release
 
 if (!(Test-Path $output))
 {
     Write-Host "File doesn't exist";
+    exit 1;
 }
 
 if (!(Test-Path ($pathToKeyStore+ $keystore)))
 {
     Write-Host "Keystore doesn't exist in " + $pathToKeyStore;
+    exit 1;
 }
+
+Remove-Item -Recurse $path 
+cordova build android --release
 
 Copy-Item -Path $keystore -Destination $path -Force
 
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ($path + $keystore) $output 
-jarsigner -verify -verbose -certs $output
 
 $AndroidPath = Get-ChildItem $android | Sort Name -Descending | Select -First 1 
 
@@ -31,3 +33,4 @@ $AndroidVersion = $android + "\" + $AndroidPath.Name
 Clear-Host
 
 cmd /c $AndroidVersion\zipalign -v 4  $output ($path + $outputApkName)
+exit 0;
